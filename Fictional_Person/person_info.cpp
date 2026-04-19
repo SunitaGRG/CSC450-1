c1397156c4fa9a9b5651a30f8e60419c290aa153
Sunita Gurung <sunita.gurung@csuglobal.edu>
2026-04-19 13:51:19 -0400

    Assignment 1 - C++

---
+++ Fictional_Person/person_info.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName   = "Mike";
+    string lastName    = "Tyson";
+    string streetAddr  = "123 Las Vegas Drive";
+    string city        = "Las Vegas";
+    string zipCode     = "89109";
+
+    cout << "First Name    : " << firstName  << endl;
+    cout << "Last Name     : " << lastName   << endl;
+    cout << "Street Address: " << streetAddr << endl;
+    cout << "City          : " << city       << endl;
+    cout << "Zip Code      : " << zipCode    << endl;
+    return 0;
+}
